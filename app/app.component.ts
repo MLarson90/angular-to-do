79bdb02f247f83465be4b35460e9c38329c2587e
Max Larson <mr.m.larson90@gmail.com>
2017-05-01 11:02:53 -0700

    an edit, finish, and color to task list

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
       <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
       <h3>{{currentFocus}}</h3>
       <ul>
-      <li *ngFor="let currentTask of tasks">{{currentTask.description}}</li>
+      <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask()">Edit</button><button (click)="finishTask(currentTask)">Done</button></li>
     </ul>
   </div>
   `
@@ -20,14 +20,35 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   tasks: Task[] = [
-   new Task('Finish weekend Angular homework for Epicodus course'),
-   new Task('Begin brainstorming possible JavaScript group projects'),
-   new Task('Add README file to last few Angular repos on GitHub')
+   new Task('Finish weekend Angular homework for Epicodus course',3),
+   new Task('Begin brainstorming possible JavaScript group projects',2),
+   new Task('Add README file to last few Angular repos on GitHub',1)
  ];
-
+  editTask(){
+    alert("Boom!");
+  }
+  isDone(clickedTask: Task) {
+    if(clickedTask.done === true){
+      alert('done');
+    }else{
+      alert('not done');
+    }
+  }
+  finishTask(clickedTask: Task){
+    clickedTask.done = true;
+  }
+  priorityColor(currentTask){
+  if (currentTask.priority === 3){
+    return "bg-danger";
+  } else if (currentTask.priority === 2) {
+    return  "bg-warning";
+  } else {
+    return "bg-info";
+  }
+}
 }
   export class Task {
   public done: boolean = false;
-  constructor(public description: string) { }
+  constructor(public description: string, public priority: number) { }
 
 }
